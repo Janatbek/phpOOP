b618ae02533a799369d00d050bd54980c501a786
Janatbek <janatbek@gmail.com>
2016-07-14 11:07:05 -0700

    linked list PHP

--- deckOfCards/index.php
+++ deckOfCards/index.php
@@ -1,48 +1,38 @@
 <?php
-	// 1. Function to have deck of cards:
 
-	// function makeAdeck() {
-	// 	$suits = array('Clubs', 'Diamonds', 'Hearts', 'Spades');
-	// 	$values = array('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King');
-	// 	$listOfCards = array();
-	// 	$faces = array();
-		
-	// 	foreach ($suits as $suit) {
-	// 		foreach ($values as $value) {
-	// 			//var_dump($suit);
-	// 			$listOfCards[] = array("suit" => $suit, "value" => $value);
-				
-
-	// 		}		
-	// 	}
-	// 	return $listOfCards;
-	// 	//gave me back a deck of cards.
-	// }
-	// $deck = makeAdeck();
-
+class Deck{
+	public $standardCards = array(); 
+	public $faces = array('Clubs', 'Diamonds', 'Hearts', 'Spades');
+	public $values = array('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King');
 	
-
-	// 2. Creating class Deck:
-
-	class Deck{
-		public $standardCards = array(); 
-		public $faces = array('Clubs', 'Diamonds', 'Hearts', 'Spades');
-		public $values = array('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King');
-		
-		public function makeAdeck() {
-			foreach ($this->faces as $face) {
-				foreach ($this->values as $value) {
-					//var_dump($face);
-					$this->standardCards[] = array("face" => $face, "value" => $value);
-				}		
-			}
-			return $this->standardCards;
+	public function makeAdeck() {
+		foreach ($this->faces as $face) {
+			foreach ($this->values as $value) {
+				//var_dump($face);
+				$this->standardCards[] = array("face" => $face, "value" => $value);
+			}		
 		}
-		public function __construct(){
-			$this->standardCards = $this->makeAdeck();
-		}
-	}//end of creting fase of Decl class.
-	
+		return $this->standardCards;
+	}
+
+	public function __construct(){
+		$this->standardCards = $this->makeAdeck();
+	}
+
+}
+$test = new Deck();
+//var_dump($test->standardCards);
+//var_dump($test);
+//end of creting fase of Decl class.
+
+function shuffling($test){
+	shuffle($test);
+	foreach ($test as $value) {
+		echo "jan";
+	}
+}
+shuffling($test->standartCards);
+
 //=====================================
 	//shuffling
 	// $numbers = range(1, 52);
